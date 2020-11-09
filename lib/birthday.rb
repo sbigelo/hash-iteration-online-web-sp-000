7fb0f1dfd9d48ce7472c3581cbeb5f4e1992adbb
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-09 18:59:44 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # birthday_kids = {
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
@@ -5,7 +6,18 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |key, value|
+    puts "Happy Birthday #{key}! You are now #{value} years old!"
+  end
 end
 
+def age_appropriate_birthday(birthday_kids)
+   birthday_kids.each do |key, value|
+     if value <= 12
+       puts "Happy Birthday #{key}! You are now #{value} years old!"
+    else
+      puts "You are too old for this."
+   end
+  end
+end
 
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,13 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |key, value|
+    
+    if key == :suite_a && value.start_with?("A")
+      
+      winner = value
+    end
+  end
+  winner
 end
